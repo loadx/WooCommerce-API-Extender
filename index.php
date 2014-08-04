2d822121ca54f4737e20fdf30d3b12b5ece18c84
Mat Brennan <loadx.mat@gmail.com>
2014-08-05 00:17:17 +1000

    Now throws an error in wp-admin when the plugin is still active/installed after WooCommerce version 2.2. As a safety guard, all plugin behaviour is disabled when this scenario occurs.

--- index.php
+++ index.php
@@ -16,17 +16,28 @@ if (!defined('ABSPATH')){
     exit; // Exit if accessed directly
 }
 
+$woo_plugin_data = get_plugins('/'.'woocommerce');
+$reject_plugin = floatval($woo_plugin_data[0]['Version']) >= 2.2 ? true : false;
+
 $routes = array(
     '/customers/email/(?P<email>.+)' => array(
         array('get_customer_by_email', WC_API_SERVER::READABLE)
     )
 );
 
-add_action('woocommerce_api_loaded', 'inject_routes', 0);
 
+/*
+inject additional routes into woocommerce's api
+*/
 function inject_routes(){
     global $wp;
     global $routes;
+    global $reject_plugin;
+
+    if($reject_plugin){
+        // woocommerce >= 2.2 and user has not yet removed the plugin. Disable functionality
+        return;
+    }
 
     foreach($routes as $route => $handler){
         $match = preg_match( '@^' . $route . '$@i', urldecode( $wp->query_vars['wc-api-route']));
@@ -35,7 +46,26 @@ function inject_routes(){
             WC()->api->register_resources(WC()->api->server);
             WC()->api->server->serve_request();
             exit();
-
         }
     }
 }
+
+
+/*
+Notifies the user when this plugin is no longer required
+ */
+function show_version_warning(){
+    global $reject_plugin;
+
+    // only show this to admins
+    if($reject_plugin && current_user_can('manage_options')){
+        echo '
+        <div class="error">
+          <p><a style="text-decoration: none;" target="_blank" href="https://github.com/ExtensionWorks/WooCommerce-API-Extender">WooCommerce-API-Extender</a> - You do not require this plugin to be installed as your <strong>WooCommerce version is >= 2.2</strong></p>
+          <span style="color: #dd3d36;"><a style="color: #dd3d36;" href="http://www.extensionworks.com">ExtensionWorks</a> advises you to remove this plugin as it is no longer needed and we have disabled it\'s functionality to prevent possible WooCommerce API issues.</span>
+        </div>';
+    }
+}
+
+add_action('woocommerce_api_loaded', 'inject_routes', 0);
+add_action('admin_notices', 'show_version_warning');
